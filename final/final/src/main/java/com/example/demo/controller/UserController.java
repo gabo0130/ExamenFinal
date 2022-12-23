5cdadeca1924deddb22e91fed6361c55f57a4b9a
gabo0130 <yoonny0130@hotmail.com>
2022-12-23 12:17:48 -0500

    ultimo comit por si acaa, prueba de edit commit

--- final/final/src/main/java/com/example/demo/controller/UserController.java
+++ final/final/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.service.UserService;
@@ -11,7 +12,9 @@ import com.example.demo.service.UserService;
 @CrossOrigin
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    private UserServiceImpl userServiceImpl;
+
+
 
     @PostMapping
     public String registrarCuentaDeUsuario(@ModelAttribute("user") User user) {
